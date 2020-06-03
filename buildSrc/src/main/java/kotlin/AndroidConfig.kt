0eb94fdf675f422fac3d3340786d2b480301aa98
Artem Tomilov <scraplesh@gmail.com>
2020-06-03 22:52:00 +0300

    Version updated.

--- buildSrc/src/main/java/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/java/kotlin/AndroidConfig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
 
-    const val versionCode = 1
-    const val versionName = "2020.05.001"
+    const val versionCode = 2
+    const val versionName = "2020.05.002"
 
     const val applicationId = "me.scraplesh.hotsdraft"
 }
